936f544fc421756148ed05078f88364669e73779
YugandharRepo <yugandhar.g414@gmail.com>
2025-11-11 11:24:17 +0530

    newchnages111125

--- src/shade/Sidebar/SideMenu.js
+++ src/shade/Sidebar/SideMenu.js
@@ -3,7 +3,7 @@ export const MENUITEMS = [
     menutitle: "Main",
     Items: [
       {
-        title: "Dashboard",
+        title: <span style={{ color: "#082038" }}>Dashboard</span>,
         icon: (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -12,19 +12,16 @@ export const MENUITEMS = [
             height="24"
             viewBox="0 0 24 24"
           >
-            <path d="M3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707l-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13zm7 7v-5h4v5h-4zm2-15.586 6 6V15l.001 5H16v-5c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v5H6v-9.586l6-6z" />
+            <path
+              d="M10 3H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM9 9H5V5h4v4zm11-6h-6a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-1 6h-4V5h4v4zm-9 4H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1zm-1 6H5v-4h4v4zm8-6c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"
+              style={{ fill: "#082038" }}
+            />
           </svg>
         ),
-        type: "sub",
-        selected: false,
-        active: false,
-        // children: [
-        // {
         path: `${process.env.PUBLIC_URL}/dashboard/dashboard-1`,
         type: "link",
         active: false,
         selected: false,
-        title: "Dashboard",
       },
       // {
       //   path: `${process.env.PUBLIC_URL}/dashboard/dashboard-2`,
